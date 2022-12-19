2915f8489630f0277fff367fc75080178905a53f
robert.makyla <robert.makyla@gmail.com>
2022-12-19 21:11:36 +0100

    cats effects playground - IO exercises

--- src/main/scala/my/playground/Part2_IO.scala
+++ src/main/scala/my/playground/Part2_IO.scala
@@ -43,5 +43,47 @@ object Part2_IO {
     greeting.unsafeRunSync()
   }
 
-  //teraz IO Exercise https://rockthejvm.com/courses/1479486/lectures/33924538
+  /**
+   * Exercises
+   */
+
+  // 1 - sequence two IOs and take the result of the LAST one
+  // hint: use flatMap
+  //
+  //   def sequenceTakeLast[A, B](ioa: IO[A], iob: IO[B]): IO[B] = ???
+
+
+  // 2 - sequence two IOs and take the result of the FIRST one
+  // hint: use flatMap
+  //
+  //   def sequenceTakeFirst[A, B](ioa: IO[A], iob: IO[B]): IO[A] = ???
+
+
+  // 3 - repeat an IO effect forever
+  // hint: use flatMap + recursion
+  //
+  //   def forever[A](io: IO[A]): IO[A] = ???
+
+
+  // 4 - convert an IO to a different type
+  // hint: use map
+  //   def convert[A, B](ioa: IO[A], value: B): IO[B] = ???
+
+
+  // 5 - discard value inside an IO, just return Unit
+  //    def asUnit[A](ioa: IO[A]): IO[Unit] = ???
+
+
+  // 6 - fix stack recursion - make it tail recursion
+  //   old way: def sum(n: Int): Int = if (n <= 0) 0 else n + sum(n - 1)
+  //
+  //   def sumIO(n: Int): IO[Int] = ???
+
+
+  // 7 (hard) - write a fibonacci IO that does NOT crash on recursion
+  // hints: use recursion, ignore exponential complexity, use flatMap heavily
+  //
+  //   def fibonacci(n: Int): IO[BigInt] = ???
+
+
 }
